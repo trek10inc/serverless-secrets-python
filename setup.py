f53ff188ac3fd8d4ea214b95d237a6aded0a3267
Ken Winner <kwinner@trek10.com>
2017-08-21 10:57:24 -0400

    Fixing package inclusion

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-version = '0.2.3'
+version = '0.2.4'
 
 setup(
   name = 'serverless_secrets',
-  packages = ['serverless_secrets'],
+  packages = find_packages(),
   version = version,
   description = 'A library for accessing secrets for use in serverless applications',
-  author = 'Trek10',
-  author_email = 'kwinner@trek10.com',
+  author = 'Trek10, Inc',
+  author_email = 'package-management@trek10.com',
   url = 'https://github.com/trek10inc/serverless-secrets-python',
   download_url = 'https://github.com/trek10inc/serverless-secrets-python/archive/' + version + '.tar.gz',
   keywords = ['serverless', 'secrets'],
