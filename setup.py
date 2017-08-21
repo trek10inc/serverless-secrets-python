7701d2d88f7b2bd8fe1aeb8adedabdadcd93d5f8
Ken Winner <kwinner@trek10.com>
2017-08-21 10:20:27 -0400

    Fixing an import for 0.2.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'serverless_secrets',
   packages = ['serverless_secrets'],
-  version = '0.1.0',
+  version = '0.2.0',
   description = 'A library for creating secrets for use in serverless applications',
   author = 'Trek10',
   author_email = 'kwinner@trek10.com',
   url = 'https://github.com/trek10inc/serverless-secrets-python',
-  download_url = 'https://github.com/trek10inc/serverless-secrets-python/archive/0.1.0.tar.gz',
+  download_url = 'https://github.com/trek10inc/serverless-secrets-python/archive/0.2.0.tar.gz',
   keywords = ['serverless', 'secrets'],
   classifiers = [],
 )
